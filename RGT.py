889475976c6032f7c9fd0c61b9e3abb514699077
GraphDetec <117416745+GraphDetec@users.noreply.github.com>
2022-11-29 16:30:06 +0800

    Update RGT.py

--- RGT.py
+++ RGT.py
@@ -16,8 +16,8 @@ warnings.filterwarnings('ignore')
 
 parser = argparse.ArgumentParser(description='RGT')
 parser.add_argument('--task', type=str, default='bot', help='detection task of stance or bot')
-parser.add_argument('--relation_select', type=list, default=[0,1], help='selection of relations in the graph (0-6)')
-parser.add_argument('--random_seed', type=list, default=[1,2,3,4,5], help='selection of random seeds')
+parser.add_argument('--relation_select', type=int, default=[0,1], nargs='+', help='selection of relations in the graph (0-6)')
+parser.add_argument('--random_seed', type=int, default=[1,2,3,4,5], nargs='+', help='selection of random seeds')
 parser.add_argument('--hidden_dimension', type=int, default=128, help='number of hidden units')
 parser.add_argument("--out_channel", type=int, default=64, help="out channels")
 parser.add_argument('--trans_head', type=int, default=4, help='number of trans_head')
@@ -103,7 +103,7 @@ def main(seed):
               'loss_train: {:.4f}'.format(loss_train.item()),
               'acc_train: {:.4f}'.format(acc_train.item()),
               'acc_val: {:.4f}'.format(acc_val.item()), )
-        return acc_train, loss_train
+        return acc_val
 
 
     def test():
@@ -119,13 +119,14 @@ def main(seed):
         return acc_test, loss_test, f1, precision, recall
 
 
-    max_acc = 0
+    max_val_acc = 0
     for epoch in range(args.epochs):
-        train(epoch)
+        acc_val = train(epoch)
         acc_test, loss_test, f1, precision, recall = test()
-        if acc_test > max_acc:
+        if acc_val > max_val_acc:
+            max_val_acc = acc_val
             max_acc = acc_test
-            max_epoch = epoch
+            max_epoch = epoch + 1
             max_f1 = f1
             max_precision = precision
             max_recall = recall
